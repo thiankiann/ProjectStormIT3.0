4ab24ef2fc6a38868320200cc9d1f92026ed3bed
@thiankiann <thiankiann@gmail.com>
2022-04-24 14:19:19 +0100

    [add] Initial Commit

---
+++ src/main/java/pl/stormit/demo/ProjectStormIt3Application.java
@@ -0,0 +1,13 @@
+package pl.stormit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStormIt3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectStormIt3Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/stormit/demo/ProjectStormIt3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.stormit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectStormIt3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
